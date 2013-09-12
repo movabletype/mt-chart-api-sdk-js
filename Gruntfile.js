95a61dec5ee4e856eb88c7a4a13017bedc86f7aa
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-09-12 16:36:11 +0900

    referred grunt.option optionally

--- Gruntfile.js
+++ Gruntfile.js
@@ -144,6 +144,7 @@ module.exports = function (grunt) {
     },
     karma: {
       jasmine: {
+        browsers: grunt.option('browser') ? [grunt.option('browser')] : ['Chrome'],
         configFile: 'karma.config.js'
       }
     }
