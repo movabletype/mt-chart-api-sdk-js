98c71d060c74b537bca5b4c60596c8d8b647af72
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-10-04 18:30:33 +0900

    use jQuery

--- test/spec_helpers/css_append.js
+++ test/spec_helpers/css_append.js
@@ -1,3 +1,3 @@
 var basePath = !! window.__karma__ ? '/base' : '';
-$('<link rel="stylesheet" href="' + basePath + '/lib/mtchart.css">').appendTo(document.head);
-$('<link rel="stylesheet" href="' + basePath + '/sample/css/jquery-ui.min.css">').appendTo(document.head);
+$('<link rel="stylesheet" href="' + basePath + '/lib/mtchart.css">').appendTo($('head'));
+$('<link rel="stylesheet" href="' + basePath + '/sample/css/jquery-ui.min.css">').appendTo($('head'));
